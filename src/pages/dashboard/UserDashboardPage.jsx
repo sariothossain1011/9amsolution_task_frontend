a7709cdebdc63e1cc16b3bd13aa4383ec9b713dd
Sariot Hossain <sariothossain1011@gmail.com>
2025-06-04 09:09:10 +0600

    vercel error solve

--- src/pages/dashboard/UserDashboardPage.jsx
+++ src/pages/dashboard/UserDashboardPage.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import UserDashboard from '../../components/dashboard/UserDashBoard'
 
 const UserDashboardPage = () => {
   return (
-    <>
+    <Fragment>
   <UserDashboard/>
-    </>
+    </Fragment>
   )
 }
 
